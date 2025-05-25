dd5f32c748a0dbf33916c33dec1e87c39c16f4c4
azalia17 <azaliaaps17@gmail.com>
2025-05-25 18:39:41 +0700

    feat: simplified design

--- light/ViewController.swift
+++ light/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var lightButton: UIButton!
+//    @IBOutlet var lightButton: UIButton!
     var lightOn = true
 
     override func viewDidLoad() {
@@ -17,13 +17,14 @@ class ViewController: UIViewController {
     }
 
     fileprivate func updateUI() {
-        if lightOn {
-            view.backgroundColor = .white
-            lightButton.setTitle("Off", for: .normal)
-        } else {
-            view.backgroundColor = .black
-            lightButton.setTitle( "On", for: .normal)
-        }
+        view.backgroundColor = lightOn ? .white : .black
+//        if lightOn {
+//            view.backgroundColor = .white
+//            lightButton.setTitle("Off", for: .normal)
+//        } else {
+//            view.backgroundColor = .black
+//            lightButton.setTitle( "On", for: .normal)
+//        }
     }
     
     @IBAction func buttonPressed(_ sender: Any) {
