c1aac474d9bff7f2f38a3a60001fe0bece9780e8
azalia17 <azaliaaps17@gmail.com>
2025-05-26 19:36:38 +0700

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Azalia Amanda on 26/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var forgotUsernameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let sender = sender as? UIButton else { return }
+        
+        if sender == forgotPasswordButton {
+            segue.destination.navigationItem.title = "Forgot Password"
+        } else if sender == forgotUsernameButton {
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else {
+            segue.destination.navigationItem.title = usernameTextField.text
+        }
+        
+    }
+
+    @IBAction func forgotPasswordTapped(_ sender: Any) {
+        performSegue(withIdentifier: "ForgottenUsernameOrPassword", sender: sender)
+    }
+    
+    @IBAction func forgotUsernameTapped(_ sender: Any) {
+        performSegue(withIdentifier: "ForgottenUsernameOrPassword", sender: sender)
+    }
+}
+
