df09a73c5f4d22a82970ca2cc96bd924937eec5c
azalia17 <azaliaaps17@gmail.com>
2025-05-26 18:24:13 +0700

    feat: passing title for next view based on textfield.text

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,5 +20,9 @@ class ViewController: UIViewController {
         
     }
 
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = textField.text
+    }
 }
 
