ecbd8d4e27fd8792fd9bdf94fab98a649d8ec331
azalia17 <azaliaaps17@gmail.com>
2025-05-26 17:47:38 +0700

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Azalia Amanda on 26/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+}
+
