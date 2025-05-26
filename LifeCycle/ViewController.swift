cd46c371e9d649ff574baa5ea35c29a8099b84bb
azalia17 <azaliaaps17@gmail.com>
2025-05-27 05:30:24 +0700

    Initial Commit

---
+++ LifeCycle/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by Azalia Amanda on 26/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("ViewController = View Did Load")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("ViewController - View Will Appear")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("ViewController - View did Appear")
+    }
+}
+
